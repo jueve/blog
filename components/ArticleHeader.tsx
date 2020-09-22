0e0a85e5ea271686ff3a7697decedc3e2a10f84b
jueve <o8g7no9q@gmail.com>
2020-09-22 18:25:18 +0900

    記事の日付欄を変更

--- components/ArticleHeader.tsx
+++ components/ArticleHeader.tsx
@@ -5,7 +5,7 @@ export function ArticleHeader(articleSummary: ArticleSummary): JSX.Element {
   return (
     <div className="mb-10">
       <h1 className="text-3xl font-bold">{articleSummary.title}</h1>
-      <p className="xt-xl date">{articleSummary.date}</p>
+      <span className="text-xl date">{articleSummary.date}</span>
     </div>
   );
 }
