690380f048371046a8068f8e7cfec6aaf417ca7f
jueve <o8g7no9q@gmail.com>
2020-09-22 13:56:31 +0900

    marginの調整

--- components/ArticleHeader.tsx
+++ components/ArticleHeader.tsx
@@ -5,7 +5,7 @@ export function ArticleHeader(articleSummary: ArticleSummary): JSX.Element {
   return (
     <div className="mb-10">
       <h1 className="text-3xl font-bold">{articleSummary.title}</h1>
-      <p className="text-xl mt-2">{articleSummary.date}</p>
+      <p className="xt-xl date">{articleSummary.date}</p>
     </div>
   );
 }
--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function HomeHeader(): JSX.Element {
   return (
-    <div className="mb-5 mt-10">
+    <div className="mb-10 mt-10">
       <div className="flex">
         <div className="flex-initial">
           <img className="rounded-full mr-2" src="/assets/profile/icon.png" alt="avatar of cashitsuki" />
@@ -13,12 +13,12 @@ export function HomeHeader(): JSX.Element {
         </div>
       </div>
       <div className="mt-2 flex">
-        <div className="pr-2 pb-2 mr-2">
+        <div className="mr-3">
           <Link href="/">
             <a className="text-xl text-blue-600 underline">Home</a>
           </Link>
         </div>
-        <div className="pr-2 pb-2 mr-2">
+        <div className="mr-2">
           <Link href="/about">
             <a className="text-xl text-blue-600 underline">About</a>
           </Link>
