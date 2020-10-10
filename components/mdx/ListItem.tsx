77091a45bffbfc30213d6eb0e5731c67e9cb31d6
jueve <o8g7no9q@gmail.com>
2020-10-10 18:56:51 +0900

    line-heightを変更

--- components/mdx/ListItem.tsx
+++ components/mdx/ListItem.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { MDXProviderProps } from "@mdx-js/react";
 
 export function ListItem(props: MDXProviderProps): JSX.Element {
-  return <li className="mb-3 leading-6">{props.children}</li>;
+  return <li className="mb-3 leading-7">{props.children}</li>;
 }
--- components/mdx/Paragraph.tsx
+++ components/mdx/Paragraph.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { MDXProviderProps } from "@mdx-js/react";
 
 export function Paragraph(props: MDXProviderProps): JSX.Element {
-  return <p className="mt-6 mb-6 text-md leading-6">{props.children}</p>;
+  return <p className="mt-6 mb-6 text-md leading-7">{props.children}</p>;
 }
