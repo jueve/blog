357d0c2ead0f324440eacf043f92fd54911e8c6a
jueve <o8g7no9q@gmail.com>
2020-10-10 18:19:51 +0900

    スタイルを修正

--- components/ArticleSummary.tsx
+++ components/ArticleSummary.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export function ArticleSummary(summary: Summary): JSX.Element {
   return (
-    <div className="mb-4">
+    <div className="mb-8">
       <p className="strong">{summary.date}</p>
       <Link href={`/article/${summary.slug}`}>
-        <a className="text-lg text-blue-600 underline">{summary.title}</a>
+        <a className="text-xl text-blue-700">{summary.title}</a>
       </Link>
     </div>
   );
