41be0b50c827ac2c2a15e356fe2a951a42b5451d
jueve <o8g7no9q@gmail.com>
2020-10-18 10:27:05 +0900

    デザインの変更

--- components/mdx/ListItem.tsx
+++ components/mdx/ListItem.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { MDXProviderProps } from "@mdx-js/react";
 
 export function ListItem(props: MDXProviderProps): JSX.Element {
-  return <li className="mb-3 leading-7">{props.children}</li>;
+  return <li className="mt-3 leading-7">{props.children}</li>;
 }
