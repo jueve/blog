37c2081a9fb602880771bc31360b713ebef45475
jueve <o8g7no9q@gmail.com>
2020-10-21 07:16:17 +0900

    デザインの変更

--- components/mdx/OrderedList.tsx
+++ components/mdx/OrderedList.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { MDXProviderProps } from "@mdx-js/react";
 
 export function OrderedList(props: MDXProviderProps): JSX.Element {
-  return <ol className="list-decimal ml-10 mb-10">{props.children}</ol>;
+  return <ol className="list-decimal ml-10 mt-8 mb-10">{props.children}</ol>;
 }
--- components/mdx/UnorderedList.tsx
+++ components/mdx/UnorderedList.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { MDXProviderProps } from "@mdx-js/react";
 
 export function UnorderedList(props: MDXProviderProps): JSX.Element {
-  return <ul className="list-disc ml-10 mb-10">{props.children}</ul>;
+  return <ul className="list-disc ml-10 mt-8 mb-10">{props.children}</ul>;
 }
